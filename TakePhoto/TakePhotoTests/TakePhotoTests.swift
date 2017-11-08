30fa525abd1a66e054e6890822899c67130568ba
lkb_shidaiwanwang <shidaiwangwang_lkb@163.com>
2017-11-08 18:13:37 +0800

    dasda

---
+++ TakePhoto/TakePhotoTests/TakePhotoTests.swift
@@ -0,0 +1,36 @@
+//
+//  TakePhotoTests.swift
+//  TakePhotoTests
+//
+//  Created by Apple on 2017/11/8.
+//  Copyright © 2017年 李康滨,工作qq:1218773641. All rights reserved.
+//
+
+import XCTest
+@testable import TakePhoto
+
+class TakePhotoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
