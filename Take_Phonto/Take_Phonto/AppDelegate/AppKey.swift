f448ad92e62014f253a37babac4d1e26f6d9e2cc
luozhuang <luozhuang@192.168.204.128>
2017-12-06 00:43:51 +0800

    把短信简单处理了一下

---
+++ Take_Phonto/Take_Phonto/AppDelegate/AppKey.swift
@@ -0,0 +1,34 @@
+//
+//  AppKey.swift
+//  Take_Phonto
+//
+//  Created by luozhuang on 2017/12/6.
+//  Copyright © 2017年 lkb. All rights reserved.
+//
+
+import Foundation
+
+
+class AppKey: NSObject {
+    
+    var shareAppkey = "22d93a8c47fcd"
+    var sahreAppsectert = "58cd0b0ef54b3d2a994373a58c03924b"
+    
+    
+    
+    
+    
+    
+}
+
+
+extension AppKey  {
+
+
+
+
+
+
+
+}
+
