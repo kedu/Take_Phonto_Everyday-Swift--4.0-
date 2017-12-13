acc7e3b57a211e75a126f96458e027f506ee0f0c
lkb_shidaiwanwang <shidaiwangwang_lkb@163.com>
2017-12-13 10:17:34 +0800

    控制器创建

---
+++ Take_Phonto/Take_Phonto/Moduel/SendPost/SendPostVC.swift
@@ -0,0 +1,27 @@
+//
+//  SendPostVC.swift
+//  Take_Phonto
+//
+//  Created by Apple on 2017/12/13.
+//  Copyright © 2017年 lkb. All rights reserved.
+//
+
+import UIKit
+
+class SendPostVC: UIViewController {
+    
+    //上传图片
+    //文本编辑
+    //发送按钮
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+
+
+}
